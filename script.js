a88a5fdbe8e98a4106f191792b3fd035542e5f9d
muzaach43-pixel <muzaach43@gmail.com>
2025-10-21 18:24:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const tombolSapa=document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function() {
   alert('Welcome To Store!,Terima Kasih Sudah Berkunjung!');
 });
+const tombolSapa=document.getElementById('sapaButton');
+tombolSapa.addEventListener('click',function() {
+  alert('Welcome To Store!,Terima Kasih Sudah Berkunjung!');
+});
+
 
 // script.js — interaksi sederhana untuk navbar, modal, dan form
 document.addEventListener('DOMContentLoaded', ()=>{
